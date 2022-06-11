01a31e25fb8a59817228e18d0851de32e8ae6c17
Xwilarg <xwilarg@protonmail.com>
2022-06-11 02:25:09 +0200

    Add web request

--- app/src/main/java/com/xwilarg/amibuyingnestle/MainActivity.kt
+++ app/src/main/java/com/xwilarg/amibuyingnestle/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.xwilarg.amibuyingnestle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        object : Thread() {
+            override fun run() {
+                val url = URL("https://en.wikipedia.org/w/api.php?action=parse&page=List_of_Nestl%C3%A9_brands&format=json")
+                val urlConnection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                try {
+                    val content = BufferedReader(InputStreamReader(urlConnection.inputStream))
+                    val json = JSONObject(content.readText())
+                    Log.i("Debug", json.getJSONObject("parse").getJSONObject("text").getString("*"))
+                } finally {
+                    urlConnection.disconnect()
+                }
+            }
+        }.start()
     }
 }
